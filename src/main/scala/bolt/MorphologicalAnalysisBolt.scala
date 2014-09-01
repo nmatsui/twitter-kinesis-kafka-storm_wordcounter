a7b517b08a2619ea496ed65bb738593011872334
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2014-09-01 02:28:03 +0000

    rename SplitSentenceBolt to MorphologicalAnalysisBolt

--- src/main/scala/bolt/MorphologicalAnalysisBolt.scala
+++ src/main/scala/bolt/MorphologicalAnalysisBolt.scala
@@ -10,9 +10,7 @@ import org.atilika.kuromoji.Tokenizer
 import java.util.{ Map => JMap }
 import scala.collection.JavaConverters._
 
-import java.io.PrintWriter
-
-class SplitSentenceBolt extends BaseRichBolt {
+class MorphologicalAnalysisBolt extends BaseRichBolt {
   var collector:OutputCollector = _
   var tokenizer:Tokenizer = _
 
@@ -25,9 +23,7 @@ class SplitSentenceBolt extends BaseRichBolt {
     tokenizer.tokenize(tuple.getString(0)).asScala.toList
       .filter(token => List("名詞", "動詞", "形容詞", "副詞").contains(token.getPartOfSpeech.split(",")(0)))
       .map(token => if (token.isKnown) token.getBaseForm else token.getSurfaceForm)
-      .map(word => {
-        this.collector.emit(tuple, new Values(word))
-      })
+      .map(word => this.collector.emit(tuple, new Values(word)))
     this.collector.ack(tuple)
   }
   override def declareOutputFields(declarer:OutputFieldsDeclarer) {
--- src/main/scala/topology/WordCountTopology.scala
+++ src/main/scala/topology/WordCountTopology.scala
@@ -4,16 +4,14 @@ import backtype.storm.{ Config, LocalCluster }
 import backtype.storm.topology.TopologyBuilder
 import backtype.storm.tuple.Fields
 
-
 object WordCountTopology {
   def main(args:Array[String]) {
     import jp.co.tis.stc.example.spout.RandomSentenceSpout
-    import jp.co.tis.stc.example.bolt.{ SplitSentenceBolt, WordCountBolt }
-    //import jp.co.tis.stc.example.bolt.SplitSentenceBolt
+    import jp.co.tis.stc.example.bolt.{ MorphologicalAnalysisBolt, WordCountBolt }
 
     val builder = new TopologyBuilder()
     builder.setSpout("spout", new RandomSentenceSpout())
-    builder.setBolt("split", new SplitSentenceBolt()).shuffleGrouping("spout")
+    builder.setBolt("split", new MorphologicalAnalysisBolt()).shuffleGrouping("spout")
     builder.setBolt("count", new WordCountBolt()).fieldsGrouping("split", new Fields("word"))
 
     val config = new Config()
@@ -22,7 +20,5 @@ object WordCountTopology {
 
     val cluster = new LocalCluster()
     cluster.submitTopology("word-count", config, builder.createTopology())
-    //Thread.sleep(30000)
-    //cluster.shutdown()
   }
 }
