8c81933d7fca199ebf4e4ad5aaf209031a97d84d
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2014-09-08 13:50:56 +0000

    use PropertyLoader

--- src/main/scala/storm/spout/StreamSpoutFactory.scala
+++ src/main/scala/storm/spout/StreamSpoutFactory.scala
@@ -10,34 +10,30 @@ import com.amazonaws.services.kinesis.stormspout.{ KinesisSpout, KinesisSpoutCon
 
 import storm.kafka.{ KafkaSpout, SpoutConfig, ZkHosts, StringScheme }
 
-import scala.collection.JavaConverters._
+import jp.co.tis.stc.example.util.PropertyLoader
 
-object StreamSpoutFactory {
+object StreamSpoutFactory extends PropertyLoader {
   def getInstance(spoutType:String):IRichSpout = spoutType match {
     case "TEST" => new RandomSentenceSpout()
     case "KAFKA" => {
-      val prop = new java.util.Properties()
-      prop.load(this.getClass.getClassLoader.getResourceAsStream("kafka.properties"))
-      val conf = prop.asScala
+      val kafkaConf = loadProperties("kafka.properties")
 
-      val zkHosts = new ZkHosts(conf("kafka.zkHosts"))
-      val topic = conf("kafka.topic")
+      val zkHosts = new ZkHosts(kafkaConf("kafka.zkHosts"))
+      val topic = kafkaConf("kafka.topic")
       val spoutConfig = new SpoutConfig(zkHosts, topic, "/kafkastorm", topic)
       spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme())
       new KafkaSpout(spoutConfig)
     }
     case "KINESIS" => {
-      val prop = new java.util.Properties()
-      prop.load(this.getClass.getClassLoader.getResourceAsStream("kinesis.properties"))
-      val conf = prop.asScala
+      val kinesisConf = loadProperties("kinesis.properties")
 
       val clientConfig = new ClientConfiguration()
-      conf.get("http.proxyHost").filter(_.nonEmpty).map(v=>clientConfig.setProxyHost(v))
-      conf.get("http.proxyPort").filter(_.nonEmpty).map(v=>clientConfig.setProxyPort(v.toInt))
-      conf.get("http.proxyUser").filter(_.nonEmpty).map(v=>clientConfig.setProxyUsername(v))
-      conf.get("http.proxyPassword").filter(_.nonEmpty).map(v=>clientConfig.setProxyPassword(v))
+      kinesisConf.get("http.proxyHost").filter(_.nonEmpty).map(v=>clientConfig.setProxyHost(v))
+      kinesisConf.get("http.proxyPort").filter(_.nonEmpty).map(v=>clientConfig.setProxyPort(v.toInt))
+      kinesisConf.get("http.proxyUser").filter(_.nonEmpty).map(v=>clientConfig.setProxyUsername(v))
+      kinesisConf.get("http.proxyPassword").filter(_.nonEmpty).map(v=>clientConfig.setProxyPassword(v))
 
-      val kinesisSpoutConfig = new KinesisSpoutConfig(conf("kinesis.streamName"), conf("storm.zkHosts"))
+      val kinesisSpoutConfig = new KinesisSpoutConfig(kinesisConf("kinesis.streamName"), kinesisConf("storm.zkHosts"))
       new KinesisSpout(kinesisSpoutConfig, new ClasspathPropertiesFileCredentialsProvider(), clientConfig)
     }
     case _ => throw new RuntimeException("invalid type %s".format(spoutType))
