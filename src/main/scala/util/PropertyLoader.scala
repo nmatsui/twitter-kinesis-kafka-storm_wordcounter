70e3c58214d80a88631021eb935833b38f0745f6
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2014-09-08 18:46:31 +0900

    add PropertyLoader trait

---
+++ src/main/scala/util/PropertyLoader.scala
@@ -0,0 +1,13 @@
+package jp.co.tis.stc.example.util
+
+import java.util.Properties
+
+import scala.collection.JavaConverters._
+
+trait PropertyLoader {
+  def loadProperties(filename:String):Map[String, String] = {
+    val prop = new Properties()
+    prop.load(this.getClass.getClassLoader.getResourceAsStream(filename))
+    prop.asScala
+  }
+}
